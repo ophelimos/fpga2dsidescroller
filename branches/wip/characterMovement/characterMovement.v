e093e581997d843c5fdd43d821632bc16075424f
computers.wreck.your.eyesight <computers.wreck.your.eyesight@ec00501a-b1ea-11dd-99fa-ddd771690bd5>
2008-11-24 22:05:37 +0000

    Flying Mario!

--- branches/wip/characterMovement/characterMovement.v
+++ branches/wip/characterMovement/characterMovement.v
@@ -30,113 +30,48 @@ module characterMovement(clock, resetn, enable, y_position_in, jump, left_blocke
 		// drawCharacter -> characterMovement -> normalizing logic -> drawCharacter
 	// y_character_position -> characterMovement -> y_movement_position -> drawCharacter
 	
+	reg [7:0] y_movement_position;
+	
 	// Y normalizing logic
 	always @(*)
 		if (~resetn)
-			y_character_position = 0;
-		else if (y_movement_position_reg >= 103 && y_movement_position_reg < 192)
-			y_character_position = 103; // 119-16
-		else if (y_movement_position_reg >= 192 && y_movement_position_reg <= 255)
-			y_character_position = 0;
+			y_movement_position = 0;
+		else if (y_position_change_jumped >= 103 && y_position_change_jumped < 192)
+			y_movement_position = 103; // 119-16
+		else if (y_position_change_jumped >= 192 && y_position_change_jumped <= 255)
+			y_movement_position = 0;
 		else
-			y_character_position = y_movement_position_reg;
+			y_movement_position = y_position_change_jumped;
 			
 	wire [7:0] y_movement_position_reg;
 	
-	wire flipflop_update;
-	
-	flipflop y_position_holder (
-	.clock(CLOCK_50),
-	.aload(flipflop_update),
-	.data(y_movement_position),
-	.q(y_position));
-	
-	//------------------------------------------
-	// Jumping counter
-	//------------------------------------------
-	
-	wire stop_jump;
-	wire slow_jump;
-	wire [5:0] jump_factor;
-	reg jump_reset_out;
-	wire jump_reset;
-	assign jump_reset = jump_reset_out;
-	
-	always @(*)
-		if (~resetn || stop_jump)
-			jump_reset_out = 1;
-		else
-			jump_reset_out = 0;
-	
-	counter4bdown jump_rate_counter (
-	.clock(clock),
-	.cnt_en(slow_jump),
-	.sclr(jump_reset),
-	.sset(jump),
-	.q(jump_factor));
+	nBitRegister y_position_holder(
+	.D(y_movement_position), 
+	.Clock(clock), 
+	.Q(y_position), 
+	.Resetn(resetn), 
+	.Enable(flipflop_update));
+	defparam y_position_holder.n = 8;
 	
 	//------------------------------------------
-	// Jumping State Machine
+	// Jumping Instantiation
 	//------------------------------------------
 	
-	// Signals -- always reg
-	reg stop_jump_out;
-	assign stop_jump = stop_jump_out;
-	reg slow_jump_out;
-	assign slow_jump = slow_jump_out;
+	wire jumping_enable;
+	wire jumping_done;
+	wire [7:0] nowhere;
 	
-	// Jumping State flipflops
-	reg [1:0] jumping_Q, jumping_D; 
-	always @ (posedge clock or negedge resetn)
-	begin: jumping_state_FFs
-		if (!resetn)
-			jumping_Q <= 0;
-		else
-			jumping_Q <= jumping_D;
-	end
+	jumpingCalc jumping_machine(
+	.clock(clock), 
+	.enable(jumping_enable), 
+	.resetn(resetn), 
+	.Yin(y_position_change),
+	.up_blocked(up_blocked),
+	.down_blocked(down_blocked),
+	.Yout(nowhere/*y_position_change_jumped*/), 
+	.done(jumping_done));
 	
-	// Jumping State Machine
-	parameter WAIT = 0, SLOW_DOWN = 1, HIT_GROUND = 2;//states
-	always @ (*)
-	begin: jumping_state_table
-		case (jumping_Q)
-			WAIT: if (jump == 1) jumping_D <= SLOW_DOWN;
-			else jumping_D <= WAIT;
-			SLOW_DOWN: if (down_blocked == 1) jumping_D <= HIT_GROUND;
-			else if (jump_factor == 0) jumping_D <= WAIT;
-			else jumping_D <= SLOW_DOWN;
-			HIT_GROUND: jumping_D <= WAIT;
-			default: jumping_D <= 2'bx;
-		endcase
-	end
-	
-	// Jumping Datapath
-	always @ (*)
-	begin: jumping_datapath
-		case (jumping_Q)
-			WAIT: 
-				begin
-					stop_jump_out = 0;
-					slow_jump_out = 0;
-				end
-			SLOW_DOWN: 
-				begin
-					stop_jump_out = 0;
-					slow_jump_out = 1;
-				end
-			HIT_GROUND: 
-				begin
-					stop_jump_out = 1;
-					slow_jump_out = 0;
-				end
-			default:
-				begin
-					stop_jump_out = 1'bx;
-					slow_jump_out = 1'bx;
-				end
-			endcase
-	end
-	
+	assign y_position_change_jumped = y_position_change;
 	//------------------------------------------
 	// The big calculation
 	//------------------------------------------
@@ -145,14 +80,9 @@ module characterMovement(clock, resetn, enable, y_position_in, jump, left_blocke
 	parameter fall_speed = 1;
 	
 	reg [7:0] y_position_change;
-	assign y_position = y_position_change;
 	
 	always @(*)
-		if (up_blocked == 0 && down_blocked == 0)
-			y_position_change = y_position_in + fall_speed - jump_factor;
-		else if (up_blocked == 0 && down_blocked == 1)
-			y_position_change = y_position_in - jump_factor;
-		else if (up_blocked == 1 & down_blocked == 0)
+		if (down_blocked == 0)
 			y_position_change = y_position_in + fall_speed;
 		else
 			y_position_change = y_position_in;
@@ -176,14 +106,14 @@ module characterMovement(clock, resetn, enable, y_position_in, jump, left_blocke
 	end
 	
 	// characterMovement State Machine
-	parameter WAIT = 0, CALCULATE = 1, DONE = 2;//states
+	parameter WAIT_CM = 0, CALCULATE = 1, DONE = 2;//states
 	always @ (*)
 	begin: characterMovement_state_table
 		case (characterMovement_Q)
-			WAIT: if (enable == 1) characterMovement_D <= CALCULATE;
-			else characterMovement_D <= WAIT;
+			WAIT_CM: if (enable == 1) characterMovement_D <= CALCULATE;
+			else characterMovement_D <= WAIT_CM;
 			CALCULATE: characterMovement_D <= DONE;
-			DONE: if (enable == 0) characterMovement_D <= WAIT;
+			DONE: if (enable == 0) characterMovement_D <= WAIT_CM;
 			else characterMovement_D <= DONE;
 			default: characterMovement_D <= 2'bx;
 		endcase
@@ -193,7 +123,7 @@ module characterMovement(clock, resetn, enable, y_position_in, jump, left_blocke
 	always @ (*)
 	begin: characterMovement_datapath
 		case (characterMovement_Q)
-			WAIT: 
+			WAIT_CM: 
 				begin
 					done = 0;
 					flipflop_update = 0;
